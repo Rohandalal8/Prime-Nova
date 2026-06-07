55ea617d28a148237dab34bc282d7b2a18a91b5a
Rohandalal8 <rohandalal079@gmail.com>
2026-06-07 22:48:22 +0530

    initial commit

---
+++ backend/controllers/authController.js
@@ -0,0 +1,82 @@
+const User = require('../model/userModel');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const sendEmail = require('../utils/sendEmail');
+
+// Generate JWT
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+};
+
+// Register a new user
+const registerUser = async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        // Check if user already exists
+        let existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+        // Hash the password        
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        // Create new user
+        const user = User.create({ name, email, password: hashedPassword });
+        await user.save();
+        // Generate OTP
+        if (user) {
+            const otp = Math.floor(100000 + Math.random() * 900000).toString();
+            const message = `
+            Welcome to Prime Basket, ${name}!
+            Thank you for registering with us. We are excited to have you on board. To complete your registration, please use the following One-Time Password (OTP):
+            Your OTP for Prime Basket registration is: ${otp}`;
+            
+            await sendEmail(email, 'Welcome to Prime Basket - Your OTP', message); 
+            res.status(201).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+                token: generateToken(user._id)
+             });
+        } else {
+            res.status(400).json({ message: 'Invalid user data' });
+        }
+        
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+// Login user
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (user && (await bcrypt.compare(password, user.password))) {
+            res.json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+                token: generateToken(user._id)
+            });
+        } else {
+            res.status(401).json({ message: 'Invalid credentials' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+// Get all users (for admin)
+const getUsers = async (req, res) => {
+    try {
+        const users = await User.find({}).select('-password'); // Exclude password from response
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+module.exports = { registerUser, loginUser, getUsers };
