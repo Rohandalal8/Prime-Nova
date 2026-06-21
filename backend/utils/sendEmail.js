5a5d98e86a3b5cb5f42d3758cd3f646a1d383b13
Rohandalal8 <rohandalal079@gmail.com>
2026-06-21 10:49:10 +0530

    fix code

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,32 +1,22 @@
 const nodemailer = require('nodemailer');
 
-const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: Number(process.env.EMAIL_PORT),
-    secure: false,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
-});
-
-transporter.verify((error, success) => {
-    if (error) {
-        console.error("SMTP VERIFY ERROR:", error);
-    } else {
-        console.log("SMTP READY");
-    }
-});
-
 const sendEmail = async (to, subject, text) => {
     try {
+        const transporter = nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: Number(process.env.EMAIL_PORT),
+            secure: false,
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS
+            }
+        });
         const mailOptions = {
             from: process.env.EMAIL,
             to,
             subject,
             text
         };
-        
         await transporter.sendMail(mailOptions);
     } catch (error) {
         console.error('Error sending email:', error);
