a08e45f9503681abfcb28d9ef27497cfee1b35fc
Rohandalal8 <rohandalal079@gmail.com>
2026-06-21 11:08:23 +0530

    fix code

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -4,7 +4,7 @@ const sendEmail = async (to, subject, text) => {
     try {
         const transporter = nodemailer.createTransport({
             host: process.env.EMAIL_HOST,
-            port: Number(process.env.EMAIL_PORT),
+            port: process.env.EMAIL_PORT,
             secure: false,
             auth: {
                 user: process.env.EMAIL_USER,
