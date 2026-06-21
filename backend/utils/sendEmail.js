bc347fd49a8bce87eedab217544c0ddeab8b7025
Rohandalal8 <rohandalal079@gmail.com>
2026-06-21 10:02:29 +0530

    fix code

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,14 +3,14 @@ const nodemailer = require('nodemailer');
 const sendEmail = async (to, subject, text) => {
     try {
         const transporter = nodemailer.createTransport({
-            service: 'Gmail',
+            host: "smtp.gmail.com",
+            port: 587,
+            secure: false,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
             },
-            connectionTimeout: 10000, // 10 sec
-            greetingTimeout: 10000,
-            socketTimeout: 10000
+            family: 4
         });
         transporter.verify((error, success) => {
             if (error) {
